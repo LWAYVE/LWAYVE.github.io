0e35a82cb46552cf530047f47be92d0bbe2c2e37
Brad De La Salle <bdelasalle@lixar.com>
2017-07-12 16:08:17 -0400

    Add Android javadoc files

---
+++ android/docs/javadoc/assets/navtree_data.js
@@ -0,0 +1,13 @@
+var NAVTREE_DATA =
+[ [ "com.lixar.lwayve.sdk.core", "com/lixar/lwayve/sdk/core/package-summary.html", [ [ "Classes", null, [ [ "LwayveSdk", "com/lixar/lwayve/sdk/core/LwayveSdk.html", null, "" ], [ "LwayveSdkConfiguration", "com/lixar/lwayve/sdk/core/LwayveSdkConfiguration.html", null, "" ], [ "LwayveSdkConfiguration.Builder", "com/lixar/lwayve/sdk/core/LwayveSdkConfiguration.Builder.html", null, "" ] ]
+, "" ] ]
+, "" ], [ "com.lixar.lwayve.sdk.exceptions", "com/lixar/lwayve/sdk/exceptions/package-summary.html", [ [ "Exceptions", null, [ [ "InvalidSdkConfigurationException", "com/lixar/lwayve/sdk/exceptions/InvalidSdkConfigurationException.html", null, "" ], [ "SdkNotInitializedException", "com/lixar/lwayve/sdk/exceptions/SdkNotInitializedException.html", null, "" ] ]
+, "" ] ]
+, "" ], [ "com.lixar.lwayve.sdk.services.audio", "com/lixar/lwayve/sdk/services/audio/package-summary.html", [ [ "Enums", null, [ [ "PlaybackEventType", "com/lixar/lwayve/sdk/services/audio/PlaybackEventType.html", null, "" ] ]
+, "" ] ]
+, "" ], [ "com.lixar.lwayve.sdk.view", "com/lixar/lwayve/sdk/view/package-summary.html", [ [ "Classes", null, [ [ "LwayvePlaybackControlView", "com/lixar/lwayve/sdk/view/LwayvePlaybackControlView.html", null, "" ], [ "LwayvePlaybackControlView.OnPlaybackEventListener", "com/lixar/lwayve/sdk/view/LwayvePlaybackControlView.OnPlaybackEventListener.html", null, "" ] ]
+, "" ] ]
+, "" ] ]
+
+;
+
