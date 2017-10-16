94ceb1ea1fb3f1af3eb1bff55ef5b3d3f2215fc7
Brad De La Salle <bdelasalle@lixar.com>
2017-10-16 15:23:07 -0400

    Update javadocs

--- android/docs/javadoc/assets/navtree_data.js
+++ android/docs/javadoc/assets/navtree_data.js
@@ -1,11 +1,15 @@
 var NAVTREE_DATA =
-[ [ "com.lixar.lwayve.sdk.core", "com/lixar/lwayve/sdk/core/package-summary.html", [ [ "Classes", null, [ [ "LwayveSdk", "com/lixar/lwayve/sdk/core/LwayveSdk.html", null, "" ], [ "LwayveSdkConfiguration", "com/lixar/lwayve/sdk/core/LwayveSdkConfiguration.html", null, "" ], [ "LwayveSdkConfiguration.Builder", "com/lixar/lwayve/sdk/core/LwayveSdkConfiguration.Builder.html", null, "" ] ]
+[ [ "com.lixar.lwayve.sdk.core", "com/lixar/lwayve/sdk/core/package-summary.html", [ [ "Interfaces", null, [ [ "LwayveSdkAsyncProvider.LwayveConnectionCallback", "com/lixar/lwayve/sdk/core/LwayveSdkAsyncProvider.LwayveConnectionCallback.html", null, "" ] ]
+, "" ], [ "Classes", null, [ [ "LwayveSdk", "com/lixar/lwayve/sdk/core/LwayveSdk.html", null, "" ], [ "LwayveSdkAsyncProvider", "com/lixar/lwayve/sdk/core/LwayveSdkAsyncProvider.html", null, "" ], [ "LwayveSdkConfiguration", "com/lixar/lwayve/sdk/core/LwayveSdkConfiguration.html", null, "" ], [ "LwayveSdkConfiguration.Builder", "com/lixar/lwayve/sdk/core/LwayveSdkConfiguration.Builder.html", null, "" ] ]
+, "" ] ]
+, "" ], [ "com.lixar.lwayve.sdk.events", "com/lixar/lwayve/sdk/events/package-summary.html", [ [ "Interfaces", null, [ [ "OnPlaybackEventListener", "com/lixar/lwayve/sdk/events/OnPlaybackEventListener.html", null, "" ] ]
+, "" ], [ "Enums", null, [ [ "ExperienceEvent", "com/lixar/lwayve/sdk/events/ExperienceEvent.html", null, "" ], [ "PlaybackEvent", "com/lixar/lwayve/sdk/events/PlaybackEvent.html", null, "" ] ]
 , "" ] ]
 , "" ], [ "com.lixar.lwayve.sdk.exceptions", "com/lixar/lwayve/sdk/exceptions/package-summary.html", [ [ "Exceptions", null, [ [ "InvalidSdkConfigurationException", "com/lixar/lwayve/sdk/exceptions/InvalidSdkConfigurationException.html", null, "" ], [ "SdkNotInitializedException", "com/lixar/lwayve/sdk/exceptions/SdkNotInitializedException.html", null, "" ] ]
 , "" ] ]
-, "" ], [ "com.lixar.lwayve.sdk.services.audio", "com/lixar/lwayve/sdk/services/audio/package-summary.html", [ [ "Enums", null, [ [ "PlaybackEventType", "com/lixar/lwayve/sdk/services/audio/PlaybackEventType.html", null, "" ] ]
+, "" ], [ "com.lixar.lwayve.sdk.mediaplayer", "com/lixar/lwayve/sdk/mediaplayer/package-summary.html", [ [ "Enums", null, [ [ "ClipPlayedAction", "com/lixar/lwayve/sdk/mediaplayer/ClipPlayedAction.html", null, "" ] ]
 , "" ] ]
-, "" ], [ "com.lixar.lwayve.sdk.view", "com/lixar/lwayve/sdk/view/package-summary.html", [ [ "Classes", null, [ [ "LwayvePlaybackControlView", "com/lixar/lwayve/sdk/view/LwayvePlaybackControlView.html", null, "" ], [ "LwayvePlaybackControlView.OnPlaybackEventListener", "com/lixar/lwayve/sdk/view/LwayvePlaybackControlView.OnPlaybackEventListener.html", null, "" ] ]
+, "" ], [ "com.lixar.lwayve.sdk.view", "com/lixar/lwayve/sdk/view/package-summary.html", [ [ "Classes", null, [ [ "LwayvePlaybackControlView", "com/lixar/lwayve/sdk/view/LwayvePlaybackControlView.html", null, "" ] ]
 , "" ] ]
 , "" ] ]
 
--- android/docs/javadoc/reference/lists.js
+++ android/docs/javadoc/reference/lists.js
@@ -1,15 +1,20 @@
 var DATA = [
       { id:0, label:"com.lixar.lwayve.sdk.core", link:"com/lixar/lwayve/sdk/core/package-summary.html", type:"package" },
       { id:1, label:"com.lixar.lwayve.sdk.core.LwayveSdk", link:"com/lixar/lwayve/sdk/core/LwayveSdk.html", type:"class" },
-      { id:2, label:"com.lixar.lwayve.sdk.core.LwayveSdkConfiguration", link:"com/lixar/lwayve/sdk/core/LwayveSdkConfiguration.html", type:"class" },
-      { id:3, label:"com.lixar.lwayve.sdk.core.LwayveSdkConfiguration.Builder", link:"com/lixar/lwayve/sdk/core/LwayveSdkConfiguration.Builder.html", type:"class" },
-      { id:4, label:"com.lixar.lwayve.sdk.exceptions", link:"com/lixar/lwayve/sdk/exceptions/package-summary.html", type:"package" },
-      { id:5, label:"com.lixar.lwayve.sdk.exceptions.InvalidSdkConfigurationException", link:"com/lixar/lwayve/sdk/exceptions/InvalidSdkConfigurationException.html", type:"class" },
-      { id:6, label:"com.lixar.lwayve.sdk.exceptions.SdkNotInitializedException", link:"com/lixar/lwayve/sdk/exceptions/SdkNotInitializedException.html", type:"class" },
-      { id:7, label:"com.lixar.lwayve.sdk.services.audio", link:"com/lixar/lwayve/sdk/services/audio/package-summary.html", type:"package" },
-      { id:8, label:"com.lixar.lwayve.sdk.services.audio.PlaybackEventType", link:"com/lixar/lwayve/sdk/services/audio/PlaybackEventType.html", type:"class" },
-      { id:9, label:"com.lixar.lwayve.sdk.view", link:"com/lixar/lwayve/sdk/view/package-summary.html", type:"package" },
-      { id:10, label:"com.lixar.lwayve.sdk.view.LwayvePlaybackControlView", link:"com/lixar/lwayve/sdk/view/LwayvePlaybackControlView.html", type:"class" },
-      { id:11, label:"com.lixar.lwayve.sdk.view.LwayvePlaybackControlView.OnPlaybackEventListener", link:"com/lixar/lwayve/sdk/view/LwayvePlaybackControlView.OnPlaybackEventListener.html", type:"class" }
+      { id:2, label:"com.lixar.lwayve.sdk.core.LwayveSdkAsyncProvider", link:"com/lixar/lwayve/sdk/core/LwayveSdkAsyncProvider.html", type:"class" },
+      { id:3, label:"com.lixar.lwayve.sdk.core.LwayveSdkAsyncProvider.LwayveConnectionCallback", link:"com/lixar/lwayve/sdk/core/LwayveSdkAsyncProvider.LwayveConnectionCallback.html", type:"class" },
+      { id:4, label:"com.lixar.lwayve.sdk.core.LwayveSdkConfiguration", link:"com/lixar/lwayve/sdk/core/LwayveSdkConfiguration.html", type:"class" },
+      { id:5, label:"com.lixar.lwayve.sdk.core.LwayveSdkConfiguration.Builder", link:"com/lixar/lwayve/sdk/core/LwayveSdkConfiguration.Builder.html", type:"class" },
+      { id:6, label:"com.lixar.lwayve.sdk.events", link:"com/lixar/lwayve/sdk/events/package-summary.html", type:"package" },
+      { id:7, label:"com.lixar.lwayve.sdk.events.ExperienceEvent", link:"com/lixar/lwayve/sdk/events/ExperienceEvent.html", type:"class" },
+      { id:8, label:"com.lixar.lwayve.sdk.events.OnPlaybackEventListener", link:"com/lixar/lwayve/sdk/events/OnPlaybackEventListener.html", type:"class" },
+      { id:9, label:"com.lixar.lwayve.sdk.events.PlaybackEvent", link:"com/lixar/lwayve/sdk/events/PlaybackEvent.html", type:"class" },
+      { id:10, label:"com.lixar.lwayve.sdk.exceptions", link:"com/lixar/lwayve/sdk/exceptions/package-summary.html", type:"package" },
+      { id:11, label:"com.lixar.lwayve.sdk.exceptions.InvalidSdkConfigurationException", link:"com/lixar/lwayve/sdk/exceptions/InvalidSdkConfigurationException.html", type:"class" },
+      { id:12, label:"com.lixar.lwayve.sdk.exceptions.SdkNotInitializedException", link:"com/lixar/lwayve/sdk/exceptions/SdkNotInitializedException.html", type:"class" },
+      { id:13, label:"com.lixar.lwayve.sdk.mediaplayer", link:"com/lixar/lwayve/sdk/mediaplayer/package-summary.html", type:"package" },
+      { id:14, label:"com.lixar.lwayve.sdk.mediaplayer.ClipPlayedAction", link:"com/lixar/lwayve/sdk/mediaplayer/ClipPlayedAction.html", type:"class" },
+      { id:15, label:"com.lixar.lwayve.sdk.view", link:"com/lixar/lwayve/sdk/view/package-summary.html", type:"package" },
+      { id:16, label:"com.lixar.lwayve.sdk.view.LwayvePlaybackControlView", link:"com/lixar/lwayve/sdk/view/LwayvePlaybackControlView.html", type:"class" }
 
     ];
